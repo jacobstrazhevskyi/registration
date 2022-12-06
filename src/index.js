d04d9baca7bbe21cf3c732b593bf05f3d7c05544
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2022-12-07 00:12:59 +0200

    starting_project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+const loginInput = document.querySelector('.login-input');
+const passwordInput = document.querySelector('.password-input');
+const registerButton = document.querySelector('.button');
+
+console.log(loginInput);
+console.log(passwordInput);
+console.log(registerButton);
